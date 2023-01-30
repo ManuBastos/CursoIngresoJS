81d0875d946b9f97c10c10cac30adc433ee75312
ManuBastos <manuelbastosgonzalez@gmail.com>
2023-01-30 17:58:43 -0300

    Clase 1 30/1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,7 @@
-/*
-Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+/* Al presionar el  botón, 
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"
+Manuel Bastos Gonzalez - Div X*/
 function mostrar()
 {
 	alert("Esto funciona de maravilla");
 }
-
